6430df3ac0f892f32df18bc45785b1a1057b8680
Rajaraman <rajaraman990@gmail.com>
2020-01-13 18:46:31 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<title>Chella Kutty</title>
+  <head>
+  <style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+  </head>
+  
+<body>
+
+
+
+  
+  <h1>Hi,Welcome to Subashini.com</h1>
+<h1>Introduction:</h1>
+
+
+</body>
+</html>
